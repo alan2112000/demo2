35fec65033af7f76344a8815b811c9eabaeca936
alan2112000 <alan21120000@gmail.com>
2014-03-17 18:18:24 +0800

    first commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,55 @@
+class EventsController < ApplicationController
+	before_filter :find_event, :only => [ :show,:edit,:update,:destroy] 	
+	def index
+		@events = Event.page(params[:page]).per(2)
+	end
+
+	def new
+		@event = Event.new
+	end
+
+
+	def create 
+		@event = Event.new(event_param)
+		if @event.save
+			flash[:notice] = "event was successfully created"
+			redirect_to events_url
+		else 
+			render :action => :new 
+		end
+	end
+
+	def show
+		@page_title = @event.name
+	end
+
+	def edit 
+		@event = Event.find(params[:id])
+	end
+
+	def update 
+		if @event.update_attributes(params[:event])
+			flash[:notice] = "event was successfully update"
+			redirect_to events_url(@event)
+		else 
+			render :action => :edit
+		end
+	end
+	def destroy
+		@event.destroy
+		flash[:alert] = "event was successfully delete"
+		redirect_to :action => :index
+	end
+
+	protected 
+
+	def find_event
+		@event = Event.find(params[:id])
+	end
+
+	private 
+
+	def event_param 
+		params.require(:event).permit(:name , :description)
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo2::Application.config.secret_key_base = '6f32ba5955dae21dd4567c5abbc58125320bfa04dde0eb6b395544a8420fc0f538419ce7be09076c966616a66a228f3e9c6ee94f8424e1570638e6514ca118b6'
